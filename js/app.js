50ce25c4055fbef9fbc856604286ef5a2cf1a50d
zst <1826493691@qq.com>
2019-09-20 18:05:24 +0800

    [feat]添加选中全部功能

--- js/app.js
+++ js/app.js
@@ -2,7 +2,30 @@ let app = new Vue({
     el: '#todoApp',
     data: {
         todoText: '',
-        todos: []
+        todos: [],
+        
+    },
+    computed: {
+            active:function(){
+                return this.todos.filter(todo=>{
+                    return todo.completed==false;
+                })
+            },
+            done:function(){
+                return this.todos.filter(todo=>{
+                    return todo.completed==true;
+                })
+        },
+        allDone:{
+            get:function(){
+                return false;
+            },
+            set:function(value){
+                this.todos.forEach(todo => {
+                    todo.completed=value;
+                });
+            }
+        }
     },
     methods: {
         addTodo: function () {
@@ -15,7 +38,21 @@ let app = new Vue({
                 completed: false
             });
             this.todoText = '';
-        }
+        },
+        //没有让值同步
+        // allDone:function(){
+        //     if(!this.active.length){
+        //         this.todos.forEach(todo => {
+        //                 return;
+        //             todo.completed=false;
+        //         });
+        //     }else{
+        //         this.active.forEach(todo=>{
+        //             todo.completed=true;
+        //         });
+        //     }
+            
+        // }
     }
 
 })
